8fd09b6587149d4973de763e634fbbdd84b4975f
hanatazkiyaa <hanatazkiyaa31@gmail.com>
2023-11-02 19:21:32 +0700

    update file

---
+++ src/main/java/projectPhone/Phone.java
@@ -0,0 +1,10 @@
+package projectPhone;
+
+public interface Phone {
+    int max_volume = 100;
+    int min_volume = 0;
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
